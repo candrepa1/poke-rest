8e7dc9b07df200ef6052f88618e4941246381042
candrepa1 <candrepa1@gmail.com>
2024-10-06 13:23:20 -0500

    registration working

--- auth/authController.js
+++ auth/authController.js
@@ -1,23 +1,18 @@
-const { Pool } = require("pg");
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
 const bcrypt = require("bcryptjs");
+const { pool } = require("../connectPg");
 
-// Register a new user
 const registerUser = async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    // Check if user already exists
     const result = await pool.query("SELECT * FROM users WHERE username = $1", [
       username,
     ]);
     if (result.rows.length > 0)
       return res.status(400).json({ message: "User already exists" });
 
-    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Insert new user into the database
     await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
       username,
       hashedPassword,
